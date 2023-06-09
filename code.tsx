a85ca0d5123b5eb7b77007655fe316fd57c8d11f
Denis Kyznetsov <104186466+DenKuznets@users.noreply.github.com>
2023-06-09 13:55:35 +0300

    Update code.tsx

--- code.tsx
+++ code.tsx
@@ -1,19 +1,30 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IconContext } from "react-icons";
+import styled from "styled-components";
 
-const [showMenu, setShowMenu] = useState(false);
+const BurgerStyled = styled.div`
+    display: inline;
+    cursor: pointer;
 
- <IconContext.Provider
-                        value={{
-                            size: "1.5em",
-                            color: 'red',
-                        }}
-                    >
-                        <div
-                            onClick={() => {
-                                setShowMenu(!showMenu);
-                            }}
-                        >
-                            <RxHamburgerMenu />
-                        </div>
-</IconContext.Provider>
+    svg {
+        transition: all 0.3s ease;
+        transform: ${({ isOpen }) => isOpen && "rotate(-90deg)"};
+    }
+`;
+
+const Burger = (props) => {
+    return (
+        <IconContext.Provider
+            value={{
+                size: "1.5em",
+                color: "red",
+            }}
+        >
+            <BurgerStyled isOpen={props.isOpen} onClick={() => props.onClick()}>
+                <RxHamburgerMenu />
+            </BurgerStyled>
+        </IconContext.Provider>
+    );
+};
+
+export default Burger;
